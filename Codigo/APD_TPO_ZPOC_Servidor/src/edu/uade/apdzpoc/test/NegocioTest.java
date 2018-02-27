7f19c1ac8124eda71dd758c62e2fc37834776c44
Barbiz <barbara.zapatero@gmail.com>
2018-02-27 12:02:03 -0300

    Solucion Articulo Entity

--- Codigo/APD_TPO_ZPOC_Servidor/src/edu/uade/apdzpoc/test/NegocioTest.java
+++ Codigo/APD_TPO_ZPOC_Servidor/src/edu/uade/apdzpoc/test/NegocioTest.java
@@ -1,5 +1,7 @@
 package edu.uade.apdzpoc.test;
 
+import edu.uade.apdzpoc.excepciones.ArticuloException;
+
 public class NegocioTest {
 
     public static void main(String[] args) throws Exception {
@@ -8,8 +10,20 @@ public class NegocioTest {
         procesarOrdenCompra();
     }
 
-    //TODO Generar un pedido web de mercadería
+    //TODO Generar un pedido web de mercaderia
     private static void generarPedidoWeb() {
+    	Prueba_hbt ph= new Prueba_hbt();
+    	try {
+			ph.recuperarArticulo(10000);
+			ph.recuperarArticulo(10200);
+			ph.recuperarArticulo(10800);
+			
+		} catch (ArticuloException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	
     }
 
     //TODO Listar los pedidos a despachar
