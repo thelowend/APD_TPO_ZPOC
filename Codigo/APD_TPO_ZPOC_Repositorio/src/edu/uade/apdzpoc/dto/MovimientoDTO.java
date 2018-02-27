dcd956bfa99e43af81ee55075d235084472ff28e
mdlaojeda <mdla.ojeda@gmail.com>
2018-02-27 19:29:45 -0300

    Actualizacion de Movimiento con Pedido Ajuste OC
    Actualizacion de Movimiento con Pedido Ajuste OC

--- Codigo/APD_TPO_ZPOC_Repositorio/src/edu/uade/apdzpoc/dto/MovimientoDTO.java
+++ Codigo/APD_TPO_ZPOC_Repositorio/src/edu/uade/apdzpoc/dto/MovimientoDTO.java
@@ -3,6 +3,9 @@ package edu.uade.apdzpoc.dto;
 import java.io.Serializable;
 import java.util.Date;
 
+import edu.uade.apdzpoc.enums.CausaAjuste;
+import edu.uade.apdzpoc.enums.DestinoArticulos;
+
 
 public class MovimientoDTO implements Serializable {
 
@@ -11,11 +14,64 @@ public class MovimientoDTO implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	protected Integer idMovimiento;
-	protected Date fecha;
-	protected ArticuloDTO articulo;
-	protected int cantidad;
-	protected String estado;
+	private Integer idMovimiento;
+	private Date fecha;
+	private ArticuloDTO articulo;
+	private int cantidad;
+	private String estado;
+	
+	private OrdenCompraDTO oc;
+	private PedidoWebDTO pw;
+	private LoteDTO lote;
+	private int legajoOperador;
+	private int legajoAutorizante;
+	private DestinoArticulos destino;
+	private CausaAjuste causa;
+	
+	
+	
+	
+	
+	public MovimientoDTO(Integer idMovimiento, Date fecha, ArticuloDTO articulo, int cantidad) {
+		super();
+		this.idMovimiento = idMovimiento;
+		this.fecha = fecha;
+		this.articulo = articulo;
+		this.cantidad = cantidad;
+	}
+	
+	
+	
+	public MovimientoDTO(Integer idMovimiento, Date fecha, ArticuloDTO articulo, int cantidad, OrdenCompraDTO oc,
+			LoteDTO lote) {
+		super();
+		this.idMovimiento = idMovimiento;
+		this.fecha = fecha;
+		this.articulo = articulo;
+		this.cantidad = cantidad;
+		this.oc = oc;
+		this.lote = lote;
+	}
+	
+	
+
+
+
+	public MovimientoDTO(Integer idMovimiento, Date fecha, ArticuloDTO articulo, int cantidad, int legajoOperador,
+			int legajoAutorizante, DestinoArticulos destino, CausaAjuste causa) {
+		super();
+		this.idMovimiento = idMovimiento;
+		this.fecha = fecha;
+		this.articulo = articulo;
+		this.cantidad = cantidad;
+		this.legajoOperador = legajoOperador;
+		this.legajoAutorizante = legajoAutorizante;
+		this.destino = destino;
+		this.causa = causa;
+	}
+
+
+
 	public Integer getIdMovimiento() {
 		return idMovimiento;
 	}
@@ -46,12 +102,68 @@ public class MovimientoDTO implements Serializable {
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
-	@Override
-	public String toString() {
-		return "MovimientoDTO [idMovimiento=" + idMovimiento + ", fecha=" + fecha + ", articulo=" + articulo
-				+ ", cantidad=" + cantidad + ", estado=" + estado + "]";
+	
+	
+	public OrdenCompraDTO getOc() {
+		return oc;
+	}
+	public void setOc(OrdenCompraDTO oc) {
+		this.oc = oc;
+	}
+	public PedidoWebDTO getPw() {
+		return pw;
+	}
+	public void setPw(PedidoWebDTO pw) {
+		this.pw = pw;
+	}
+	public LoteDTO getLote() {
+		return lote;
+	}
+	public void setLote(LoteDTO lote) {
+		this.lote = lote;
+	}
+	public int getLegajoOperador() {
+		return legajoOperador;
+	}
+	public void setLegajoOperador(int legajoOperador) {
+		this.legajoOperador = legajoOperador;
+	}
+	public int getLegajoAutorizante() {
+		return legajoAutorizante;
+	}
+	public void setLegajoAutorizante(int legajoAutorizante) {
+		this.legajoAutorizante = legajoAutorizante;
+	}
+	public DestinoArticulos getDestino() {
+		return destino;
+	}
+	public void setDestino(DestinoArticulos destino) {
+		this.destino = destino;
+	}
+	public CausaAjuste getCausa() {
+		return causa;
+	}
+	public void setCausa(CausaAjuste causa) {
+		this.causa = causa;
 	}
 
+	public String toStringMovimientoPedido() {
+		return "MovimientoDTO [idMovimiento=" + idMovimiento + ", fecha=" + fecha + ", articulo=" + articulo
+				+ ", cantidad=" + cantidad + ", pw=" + pw + "]";
+	}
+
+	public String toStringMovimientoOrdenCompra() {
+		return "MovimientoDTO [idMovimiento=" + idMovimiento + ", fecha=" + fecha + ", articulo=" + articulo
+				+ ", cantidad=" + cantidad + ", oc=" + oc + ", lote=" + lote + "]";
+	}
+	public String toStringMovimientoAjuste() {
+		return "MovimientoDTO [idMovimiento=" + idMovimiento + ", fecha=" + fecha + ", articulo=" + articulo
+				+ ", cantidad=" + cantidad + ", legajoOperador=" + legajoOperador + ", legajoAutorizante="
+				+ legajoAutorizante + ", destino=" + destino + ", causa=" + causa + "]";
+	}
+	
+	
+	
 	
 	
 	
