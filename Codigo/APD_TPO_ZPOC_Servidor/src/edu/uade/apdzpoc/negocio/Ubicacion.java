1eb8f07a2d01a4936808ef21acdeffc950f15673
Diego Pablos <thelowend6@gmail.com>
2018-03-01 15:33:25 -0300

    Terminando arreglo

--- Codigo/APD_TPO_ZPOC_Servidor/src/edu/uade/apdzpoc/negocio/Ubicacion.java
+++ Codigo/APD_TPO_ZPOC_Servidor/src/edu/uade/apdzpoc/negocio/Ubicacion.java
@@ -133,6 +133,7 @@ public class Ubicacion {
 			return cantidadRestante - cantEnUbicacion; // cant articulos que falta ubicar
 		} else {
 			this.setCapacidad(cantEnUbicacion - cantidadRestante);
+			this.setEstado(EstadoUbicacion.Con_disponibilidad);
 			this.save();
 			return 0; //No hay mas articulos a ubicar.
 		}
